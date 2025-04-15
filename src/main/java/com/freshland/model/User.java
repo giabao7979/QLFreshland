e4634a52b7d03fbde5aba67759520a775f610e89
giabao7979 <dogiabao236@gmail.com>
2025-04-15 16:05:13 +0700

    Mô tả thay đổi

--- src/main/java/com/freshland/model/User.java
+++ src/main/java/com/freshland/model/User.java
@@ -1,5 +1,53 @@
 package com.freshland.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(nullable = false, unique = true)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(name = "full_name")
+    private String fullName;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.CUSTOMER;
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders = new ArrayList<>();
+    
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    
+    public enum Role {
+        ADMIN, STAFF, CUSTOMER
+    }
 }
